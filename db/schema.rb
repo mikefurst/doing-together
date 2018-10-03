fbc5a5b59939d67068ee9e717dafc7df45fdc4d5
Michael Furst <mfurst@tulane.edu>
2018-10-03 16:08:48 +0000

    Devise user configurated. Not yet plugged into website.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180925205435) do
+ActiveRecord::Schema.define(version: 20181003160625) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "actid"
@@ -23,6 +23,18 @@ ActiveRecord::Schema.define(version: 20180925205435) do
   create_table "activity_types", force: :cascade do |t|
     t.string "name"
     t.float  "score"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
