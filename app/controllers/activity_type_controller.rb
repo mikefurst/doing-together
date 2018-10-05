d3a748a1dc4605888fd6575ff2a2d8960e63675d
Michael Furst <mfurst@tulane.edu>
2018-10-05 17:48:42 +0000

    Restrict activity types by user/group ids.

--- app/controllers/activity_type_controller.rb
+++ app/controllers/activity_type_controller.rb
@@ -4,13 +4,25 @@ class ActivityTypeController < ApplicationController
     end
     def new
         @actType = ActivityType.new
-        @actTypes = ActivityType.all.sort{ |a,b| a[:name]<=>b[:name] }
+        @actTypes = ActivityType.select { |act|
+                if current_user.groupid==nil
+                    act.userid==current_user.id
+                else
+                    act.groupid==current_user.groupid
+                end
+            }.sort{ |a,b| a[:name]<=>b[:name] }
     end
     def acttype_params
         params.require(:activitytypes).permit("name","score")
     end
     def create
         @actType = ActivityType.create(acttype_params)
+        
+        if current_user.groupid==nil
+            @actType.userid=current_user.id
+        else
+            @actType.groupid=current_user.groupid
+        end
         
         if @actType.save
             redirect_to :action => 'index'
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 #list of test activities to seed to the activity db
+=begin
 activity_list = [
     {
         :name => "Walking",
@@ -39,3 +40,145 @@ activity_list = [
 activity_list.each do |act|
     Activity.create!(act)
 end
+
+activity_types = [
+  {
+      :name => "Cycling",
+      :score => 3.0
+  },
+  {
+      :name => "Jogging",
+      :score => 1.5
+  },
+  {
+      :name => "Jumping Jacks",
+      :score => 1.2
+  },
+  {
+      :name => "Running",
+      :score => 2.0
+  },
+  {
+      :name => "Swimming",
+      :score => 2.0
+  },
+  {
+      :name => "Walking",
+      :score => 1
+  }
+  
+  ]
+activity_types.each do |act|
+  ActivityType.create!(act)
+end
+=end
+
+@group = Group.create()
+@group.name = "Pirates of the Carribean"
+@group.description = "Swashbucklers of the Carribean. Seeking out legendary treasure. Train with us to become a pirate."
+@group.password = "Yohohoandabottleofrum"
+@group.save!
+
+user_list = [
+    {
+        :first_name => "Jack",
+        :last_name => "Sparrow",
+        :email => "jsparrow@theblackpearl.com",
+        :password => "JohnnyDepp",
+    },
+    {
+        :first_name => "Will",
+        :last_name => "Turner",
+        :email => "wturner@theblackpearl.com",
+        :password => "OrlandoBloom",
+    },
+    {
+        :first_name => "Elizabeth",
+        :last_name => "Swan",
+        :email => "eswan@theblackpearl.com",
+        :password => "KieraKnightley",
+    },
+    {
+        :first_name => "Hector",
+        :last_name => "Barbosa",
+        :email => "hbarbosa@theblackpearl.com",
+        :password => "GeoffreyRush",
+    }
+    ]
+    
+    
+user_list.each do |usr|
+    @user = User.create(usr)
+    @user.groupid=@group.id
+    @user.save!
+end
+
+activitytypelist = [
+    {
+        :name => "Swimming",
+        :score => 2.125,
+        :groupid => @group.id
+    },
+    {
+        :name => "Swashbuckling",
+        :score => 1.15,
+        :groupid => @group.id
+    },
+    {
+        :name => "Fencing",
+        :score => 1.85,
+        :groupid => @group.id
+    },
+    {
+        :name => "Scrubbing the Deck",
+        :score => 1.56,
+        :groupid => @group.id
+    },
+    {
+        :name => "Hoisting the Sails",
+        :score => 1.35,
+        :groupid => @group.id
+    },
+    {
+        :name => "Stealing from the Spanish",
+        :score => 4.00,
+        :groupid => @group.id
+    }
+    ]
+    
+activitytypelist.each do |atype|
+    ActivityType.create!(atype)
+end
+
+=begin
+activity_types = [
+      {
+          :name => "Cycling",
+          :score => 3.0
+      },
+      {
+          :name => "Jogging",
+          :score => 1.5
+      },
+      {
+          :name => "Jumping Jacks",
+          :score => 1.2
+      },
+      {
+          :name => "Running",
+          :score => 2.0
+      },
+      {
+          :name => "Swimming",
+          :score => 2.0
+      },
+      {
+          :name => "Walking",
+          :score => 1
+      }
+      
+      ]
+    activity_types.each do |act|
+      ActivityType.create!(act)
+    end
+=end
