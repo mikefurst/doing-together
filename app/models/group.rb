33ec9a5c262c4339a94f6afac03dd2f74e97d0da
Michael Furst <mfurst@tulane.edu>
2018-10-16 21:28:14 +0000

    Added data verification for important fields, and standardized inputs in forms.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,19 @@
 require 'bcrypt'
 class Group < ApplicationRecord
     include BCrypt
+    
+    validates :name,
+        presence: true,
+        length: 10..50,
+        on: :create,
+        allow_nil: false
+        
+    validates :description,
+        presence: true,
+        length: 15..150,
+        on: :create,
+        allow_nil: false
+    
     def password
         @password ||= Password.new(password_hash)
     end
@@ -8,9 +21,5 @@ class Group < ApplicationRecord
         @password = Password.create(new_password)
         self.password_hash = @password
     end
-    def edit
-        @group = Groups.find(params[:id])
-    end
-    def update
-    end
+    
 end
