c68429ac72a89528bf51aaeb5b67a10c59dacf91
Michael Furst <mfurst@tulane.edu>
2018-10-23 21:59:02 +0000

    Removed conflicting coffeescript file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   config.public_file_server.enabled=true
 
   # Compress JavaScripts and CSS.
-  #config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Fallback to assets pipeline if a precompiled asset is missed.
