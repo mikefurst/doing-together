86247dfa08b4bf18c9b34dec2ef7f044f6472812
Michael Furst <mfurst@tulane.edu>
2018-11-08 22:05:38 +0000

    Facebook login fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,23 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '2120004598251262',
+      cookie     : true,
+      xfbml      : true,
+      version    : '3.2'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
