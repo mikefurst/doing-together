15529339ab29ed75863021195321f8e007518a49
Jake McLennan <jmclenn@tulane.edu>
2018-11-13 01:28:27 +0000

    Fixed a bug with the message sending

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -6,12 +6,15 @@ class ConversationsController < ApplicationController
     def show
         @conversation = current_user.mailbox.conversations.find(params[:id])
     end
-    
+   
     def new
         @recipients = User.all - [current_user]
     end
     
     def create
+        recipient = User.find(params[:user_id])
+        receipt = current_user.send_message(recipient, params[:body], params[:subject])
+        redirect_to conversation_path(receipt.conversation)
     
     end
 end
