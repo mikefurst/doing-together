be1df56103770baff8f75532ba59b57d9417a286
Jake McLennan <jmclenn@tulane.edu>
2018-11-13 03:29:58 +0000

    Added ability for user to send messages to other users via clicking a button on their profile

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -8,13 +8,18 @@ class ConversationsController < ApplicationController
     end
    
     def new
+        @user = User.find_by(id: params[:user])
         @recipients = User.all - [current_user]
+        
+
+
     end
     
     def create
-        recipient = User.find(params[:user_id])
+        recipient = User.find_by(id: params[:recipient_id])
         receipt = current_user.send_message(recipient, params[:body], params[:subject])
         redirect_to conversation_path(receipt.conversation)
     
     end
+
 end
