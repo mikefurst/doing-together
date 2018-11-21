4357d29bc5cfba5076aabf2f72132fffc576ef3a
Michael Furst <mfurst@tulane.edu>
2018-11-21 22:46:07 +0000

    Bug fixes. Buzz buzz

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -46,11 +46,11 @@ class ActivityController < ApplicationController
         params.require(:activities).permit("actid","duration","userid")
     end
     def create
-        if act_params[:duration] <= 0
+        if act_params[:duration].to_f <= 0
             return false
-        elsif act_params[:userid] != current_user.id
+        elsif act_params[:userid].to_i != current_user.id
             return false
-        elsif ActivityType.find(act_param[:actid]).groupid != current_user.groupid
+        elsif ActivityType.find(act_param[:actid].to_i).groupid != current_user.groupid
             return false
         end
         @act = Activity.create(act_params)
